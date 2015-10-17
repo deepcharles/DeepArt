68b56bb171d63011a4c13f607fba0c6a5dbe348e
Massil <massil.achab@gmail.com>
2015-10-17 23:55:38 +0200

    ...

--- loader.py
+++ loader.py
@@ -1,10 +1,10 @@
 #import numpy as np
 import lasagne
-
 from vgg19 import build_model
-
 import cPickle as pickle
-model = pickle.load(open('data/vgg19.pkl'))
-CLASSES = model['synset words']
-MEAN_IMAGE = model['mean image']
-lasagne.layers.sett_all_param_values(output_layer, model['values'])
+
+def load_weights(filename, output_layer):
+	model = pickle.load(open(filename))
+	#CLASSES = model['synset words']
+	#MEAN_IMAGE = model['mean image']
+	lasagne.layers.sett_all_param_values(output_layer, model['values'])
